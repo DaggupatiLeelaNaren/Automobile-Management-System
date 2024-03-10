325a3a1d2a85b3b87b2f57276684a9f6aa2079f7
DaggupatiLeelaNaren <149412439+DaggupatiLeelaNaren@users.noreply.github.com>
2024-03-10 10:37:54 +0530

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,28 @@
+// Home.js
+import React from 'react';
+// import { Link } from 'react-router-dom';
+
+import './Home.css'; 
+import AppBar from './Appbar.js';
+
+
+function Home() {
+  return (
+    <div>
+      
+      <div className="home-container">
+      <AppBar position="static">
+      </AppBar>
+      </div>
+       <div className='body'>
+        
+       
+
+
+     
+      </div>
+    </div>
+  );
+}
+
+export default Home;
